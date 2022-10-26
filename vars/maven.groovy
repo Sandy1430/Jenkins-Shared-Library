32a2a58fb4b8f087d20d0f2b5b2da625adcd9d05
Sandy1430 <54204496+Sandy1430@users.noreply.github.com>
2022-10-26 20:22:28 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,3 +1,7 @@
-def mvnHome = tool name: 'maven3' , type: 'maven'
-sh '${mvnHome}/bin/mvn clean package'
-sh 'mv target/myweb*.war target/newapp.war'  
+stage ('Maven Build') {
+  steps {
+    def mvnHome = tool name: 'maven3' , type: 'maven'
+    sh '${mvnHome}/bin/mvn clean package'
+    sh 'mv target/myweb*.war target/newapp.war'  
+  }
+}
