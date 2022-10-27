dbfa641eb37f0c13da5e3a3e9f0a1120ae7ca79d
Sandy1430 <54204496+Sandy1430@users.noreply.github.com>
2022-10-27 13:14:26 +0530

    Update dockerRun.groovy

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -1,3 +1,7 @@
 def dockerRun () {
-  sh ' docker run -p "8081:8080" --name newapp DockerCredentials_USR/newapp:$BUILD_NUMBER '
+  stage ('DOCKER RUN') {
+    steps {
+      sh ' docker run -p "8081:8080" --name newapp DockerCredentials_USR/newapp:$BUILD_NUMBER '
+    }
+  }
 }
