34e251c3348c77147a9872ab2a8427be2793c6f2
Sandy1430 <54204496+Sandy1430@users.noreply.github.com>
2022-10-27 16:26:19 +0530

    Create post.groovy

---
+++ vars/post.groovy
@@ -0,0 +1,9 @@
+def postBuild () {
+  post {
+    always {
+      emailext to: 'sandycse96@gmail.com' ,
+      subject: 'Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}'
+      body: '${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\\n More info at: ${env.BUILD_URL}' , 
+    }
+  }
+}
