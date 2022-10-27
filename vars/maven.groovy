e360c12ee5f4018d26697d9a8a2f6e21cdda1491
Sandy1430 <54204496+Sandy1430@users.noreply.github.com>
2022-10-27 12:25:58 +0530

    Delete maven.groovy

--- vars/maven.groovy
+++
@@ -1,5 +0,0 @@
-def call (String name = 'User'){
-    def mvnHome = tool name: 'maven3' , type: 'maven'
-    sh '${mvnHome}/bin/mvn clean package'
-    sh 'mv target/myweb*.war target/newapp.war'  
-}
