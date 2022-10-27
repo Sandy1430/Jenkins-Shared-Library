b7226dc0d81d921b109bb446b2f30855e0843eb9
Sandy1430 <54204496+Sandy1430@users.noreply.github.com>
2022-10-27 19:34:53 +0530

    Update post.groovy

--- vars/post.groovy
+++ vars/post.groovy
@@ -1,5 +1,5 @@
 def call () {
       emailext to: 'sandycse96@gmail.com',
-      subject: 'Jenkins Build $currentBuild.currentResult: Job $JOB_NAME',
-      body: '$currentBuild.currentResult: Job $JOB_NAME build $BUILD_NUMBER\\n More info at: $BUILD_URL'
+      subject: 'Jenkins Build currentBuild.currentResult: Job $JOB_NAME',
+      body: 'currentBuild.currentResult: Job $JOB_NAME build $BUILD_NUMBER \\n More info at: $BUILD_URL'
 }
