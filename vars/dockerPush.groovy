0975b3386b70f13246f465fb9980d63bd3001a58
Sandy1430 <54204496+Sandy1430@users.noreply.github.com>
2022-10-27 16:37:39 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,6 +2,6 @@ def dockerPush () {
   stage ('DOCKER PUSH') {
       sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
       sh ' docker push $DOCKERHUB_CREDENTIALS_USR/newapp:$BUILD_NUMBER '
-      sh ' docker rmi $(docker images -a -q) '
+      sh ' docker rmi -f $(docker images -a -q) '
   }
 }
