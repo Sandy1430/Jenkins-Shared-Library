82100e5e903033421de19a3a3543a362ed456348
Sandy1430 <54204496+Sandy1430@users.noreply.github.com>
2022-10-27 16:45:02 +0530

    Update post.groovy

--- vars/post.groovy
+++ vars/post.groovy
@@ -3,7 +3,7 @@ def postBuild () {
     always {
       emailext to: 'sandycse96@gmail.com' ,
       subject: 'Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}'
-      body: '${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\\n More info at: ${env.BUILD_URL}' , 
+      body: '${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\\n More info at: ${env.BUILD_URL}'
     }
   }
 }
